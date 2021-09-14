cfe99e79a633005f8d76bed1e9fad6e79264c1be
zhangpeitao <zhangpeitao@eidop.com>
2021-09-14 11:00:48 +0800

    05

--- src/test/java/org/mybatis/spring/sample/SampleJavaConfigTest.java
+++ src/test/java/org/mybatis/spring/sample/SampleJavaConfigTest.java
@@ -2,15 +2,18 @@ package org.mybatis.spring.sample;
 
 import org.mybatis.spring.sample.config.SampleConfig;
 import org.mybatis.spring.sample.domain.SysDept;
-import org.mybatis.spring.sample.service.FooService;
+import org.mybatis.spring.sample.mapper.SysDeptMapper;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class SampleJavaConfigTest {
 
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SampleConfig.class);
-        FooService fooService = ctx.getBean(FooService.class);
-        SysDept sysDept = fooService.doSomeBusinessStuff(1297780912181121026L);
+
+        SysDeptMapper sysDeptMapper = ctx.getBean(SysDeptMapper.class);
+        SysDept sysDept = sysDeptMapper.getById(1297780912181121026L);
         System.out.println(sysDept);
     }
 }
